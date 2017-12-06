8d5add42ed0457e6f825345a81c6d1c28af65e85
christo1995 <christo@webandcrafts.com>
2017-12-06 23:32:20 +0530

    new page on new branch

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="HandheldFriendly" content="true">
+    <meta http-equiv="x-rim-auto-match" content="none">
+    <title>Al-Qatami</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link href="css/style.css" rel="stylesheet"/>
+    <link href="css/responsive.css" rel="stylesheet"/>
+    <link rel="icon" href="favicon.ico" type="image/gif">
+    <script>
+        /* minimum screen width size 400*/
+        if (screen.width < 400)
+        {
+            var mvp = document.getElementById('webviewport');
+            mvp.setAttribute('content','width=400, initial-scale=1.0');
+        }
+    </script>
+</head>
+<body>
+<div id="wrapper">
+    <!-- mobile menu -->
+    <div id="main">
+        <header>
+            <div class="navbar">
+                <div class="navbar_logo">
+                    <img src="img/logo_header.png">
+                </div>
+                <div class="navbar_menu">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Corporate</a>
+                        </li>
+                        <li>
+                            <a href="#">Business</a>
+                        </li>
+                        <li>
+                            <a href="#">Media</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <ul>
+                <li>
+            </ul>
+        </header>
+        <!-- header -->
