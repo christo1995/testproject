f58f1a8e7109e09dd717d42fad7ec9b9dac539e2
christo1995 <christo@webandcrafts.com>
2017-12-06 23:49:01 +0530

    2 changes on master

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function MyFunction(){
+	document.getElementById('demo').innerHTML='world';
+	document.getElementById('demo').style.fontSize='25px';
+}
+document.write(5+6);
+window.alert(5+6);
+console.log(5+6)
